646ba220fd8af6428db52e765f49745a65381673
Kaxon <karenaxon@users.noreply.github.com>
2022-05-16 19:55:19 -0700

    Update component

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -2,67 +2,75 @@ import React, { useState } from "react";
 import Jumbotron from "./Jumbotron";
 import PinataConnection from "./../utils/PinataConnection";
 
-const Landing = () => {
+const Landing = (props) => {
   const [pinataApiKey, setPinataApiKey] = useState("");
   const [pinataSecretKey, setPinataSecretKey] = useState("");
   let apiKey;
   let secretKey;
   const keys = {
-      key1: pinataApiKey, 
-      key2: pinataSecretKey 
-    };
+    key1: pinataApiKey,
+    key2: pinataSecretKey,
+  };
 
-  localStorage.setItem("keys", JSON.stringify(keys)  || "");
-  
-  if(pinataApiKey && pinataSecretKey){
-    return(
-        <div className="container">
-          <PinataConnection apiKey={pinataApiKey} secretKey={pinataSecretKey}/>
-        </div>
-    )
+  localStorage.setItem("keys", JSON.stringify(keys) || "");
+
+  if (pinataApiKey && pinataSecretKey) {
+    return (
+      <div className="container">
+        <PinataConnection apiKey={pinataApiKey} secretKey={pinataSecretKey} />
+      </div>
+    );
   }
 
   return (
-      <div className="container">
-        <Jumbotron />
-        <h2 className="display-6">Connect to your Pinata account</h2>
-        <form>
-          <div className="mb-3">
-            <label className="form-label">Your Pinata API Key</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Pinata API Key"
-              // value={inputField}
-              onChange={(e) => {
-                apiKey = (e.target.value);
-              }}
-            />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Your Pinata Secret API Key</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Pinata Secret API Key"
-              // value={inputField}
-              id="inputField"
-              onChange={(e) => {
-                secretKey = (e.target.value);
-              }}
-            />
-            <button 
-            type="button" 
+    <div className="container">
+      <Jumbotron />
+      {props.message ? (
+        <div className="alert alert-danger bg-opacity-25 my-2">
+          {props.message}
+        </div>
+      ) : null}
+      <h2 className="display-6">Upload | View Pins but first login</h2>
+      <p className="fw-light">
+        Your keys will be stored in local storage until you clean your cache.
+      </p>
+      <hr />
+      <form className="mt-4">
+        <div className="mb-3">
+          <label className="form-label">Your Pinata API Key</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Pinata API Key"
+            onChange={(e) => {
+              apiKey = e.target.value;
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Your Pinata Secret API Key</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Pinata Secret API Key"
+            id="inputField"
+            onChange={(e) => {
+              secretKey = e.target.value;
+            }}
+          />
+          <button
+            type="button"
             className="btn btn-dark mt-3"
             onClick={(e) => {
-              setPinataApiKey(apiKey)
-              setPinataSecretKey(secretKey)
-              // setInputField("")
-              }
-            }>Submit</button>
-          </div>
-        </form>
-      </div>
+              setPinataApiKey(apiKey);
+              setPinataSecretKey(secretKey);
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
