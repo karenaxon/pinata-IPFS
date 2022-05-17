46d10cd5239c66fff75d1c3f26adb9c1ee468b34
Kaxon <karenaxon@users.noreply.github.com>
2022-05-16 19:54:53 -0700

    Update component

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -1,24 +1,16 @@
-import React from 'react'
-import Logo from "./../assets/images/home-logo.png"
+import React from "react";
+import Logo from "./../assets/images/home-logo.png";
 
 const Jumbotron = () => {
   return (
     <>
-        <div className="container mt-3">
-          <div className="mb-2 py-2 bg-warning rounded-3">
-            <div className="container-fluid d-flex justify-content-center">
-              {
-                <img
-                  src={Logo}
-                  className="homeLogo img-fluid"
-                  alt="fake-logo"
-                ></img>
-              }
-            </div>
-          </div>
+      <div className="container my-2 py-2 bg-warning rounded-3">
+        <div className="container-fluid d-flex justify-content-center">
+          <img src={Logo} className="homeLogo img-fluid" alt="logo"></img>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Jumbotron
+export default Jumbotron;
